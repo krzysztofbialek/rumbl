7cf34a347ecfa942f924df8701aab92b58779d21
Krzysztof Białek <krzysztof.bialek@gmail.com>
2019-12-08 21:36:33 +0100

    user creation

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -7,5 +7,12 @@ defmodule Rumbl.Accounts.User do
     field :username, :string
 
     timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
   end 
 end
