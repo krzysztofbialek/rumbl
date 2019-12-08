d2ee83d50dd75e31e4dae401baa741fa24689b5e
Krzysztof Białek <krzysztof.bialek@gmail.com>
2019-12-08 01:13:33 +0100

    create user table and update Accounts.User

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -2,24 +2,22 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts Context
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Jose Maria", username: "josevalim"},
-      %User{id: "2", name: "Bruce Wayne", username: "redrad"},
-      %User{id: "3", name: "Chris Pratt", username: "hodor"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map -> 
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
   end
 end
