092d6b2b49001fa2ec970564238f949ad7f51454
Krzysztof Białek <krzysztof.bialek@gmail.com>
2020-01-12 18:44:37 +0100

    setup channel with annotations

--- assets/js/player.js
+++ assets/js/player.js
@@ -23,7 +23,7 @@ let Player = {
   },
 
   onPlayerStateChange(event) {},
-  getCurrentTime() { return Math.floot(this.player.getCurrentTime() * 1000)},
+  getCurrentTime() { return Math.floor(this.player.getCurrentTime() * 1000)},
   seekTo(millsec) { return this.player.seekTo(millsec / 1000) }
 }
 export default Player
