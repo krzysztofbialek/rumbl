22862ba1e3efca15f5c53a4459d2c245850ddbdf
Krzysztof Białek <krzysztof.bialek@gmail.com>
2020-01-20 20:58:55 +0100

    track joining and leaving channel

--- assets/js/video.js
+++ assets/js/video.js
@@ -27,10 +27,10 @@ let Video = {
     let presence = new Presence(vidChannel)
 
     presence.onSync(() => {
-      userList.innerHTML = presence.list((id, {metas: [first, ...rest]}) => {
+      userList.innerHTML = presence.list((id, {user: user, metas: [first, ...rest]}) => {
         let count = rest.length + 1
 
-        return `<li>${id}: (${count})</li>`
+        return `<li>${user.username}: (${count})</li>`
       }).join("")
     })
 
--- lib/rumbl_web/channels/presence.ex
+++ lib/rumbl_web/channels/presence.ex
@@ -68,6 +68,19 @@ defmodule RumblWeb.Presence do
   information, while maintaining the required `:metas` field from the
   original presence data.
   """
+
+  def fetch(_topic, entries) do
+    users = 
+    entries
+    |> Map.keys() 
+    |> Rumbl.Accounts.list_users_with_ids()
+    |> Enum.into(%{}, fn user -> { to_string(user.id), %{username: user.username} } end)
+
+    for {key, %{metas: metas}} <- entries, into: %{} do
+      {key, %{metas: metas, user: users[key]}}
+    end
+  end
+
   use Phoenix.Presence, otp_app: :rumbl,
                         pubsub_server: Rumbl.PubSub
 end
